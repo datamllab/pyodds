3f0baf26938f7cf4fec37d324e1c607f43eda224
Praveen Venugopal <razorvine@tamu.edu>
2019-12-27 22:29:10 -0600

    revert changes to demo.py

--- demo.py
+++ demo.py
@@ -2,13 +2,8 @@ import numpy as np
 import argparse
 import time
 import logging
-import pandas as pd
-import os
-import io
-import contextlib
 import getpass
 import warnings
-from sklearn.metrics import accuracy_score,precision_score,recall_score,f1_score,roc_auc_score
 from pyodds.utils.utilities import output_performance,insert_demo_data,connect_server,query_data
 from pyodds.utils.importAlgorithm import algorithm_selection
 from pyodds.utils.plotUtils import visualize_distribution_static,visualize_distribution_time_serie,visualize_outlierscore,visualize_distribution
@@ -27,7 +22,7 @@ if __name__ == '__main__':
     parser.add_argument('--random_seed',default=42, type=int)
     parser.add_argument('--database',default='db')
     parser.add_argument('--table',default='t')
-    parser.add_argument('--time_stamp',default=True,const=True,type=str2bool,nargs='?')
+    parser.add_argument('--time_stamp',const=True,type=str2bool,nargs='?')
     parser.add_argument('--visualize_distribution',default=True,const=True,type=str2bool,nargs='?')
     parser.add_argument('--algorithm',default='dagmm',choices=['iforest','lof','ocsvm','robustcovariance','staticautoencoder','luminol','cblof','knn','hbos','sod','pca','dagmm','autoencoder','lstm_ad','lstm_ed'])
     parser.add_argument('--contamination',default=0.05)
@@ -44,7 +39,7 @@ if __name__ == '__main__':
     rng = np.random.RandomState(args.random_seed)
     np.random.seed(args.random_seed)
 
-    password = "taosdata" #getpass.getpass("Please input your password:")
+    password = getpass.getpass("Please input your password:")
 
     #connection configeration
     conn,cursor=connect_server(args.host, args.user, password)
@@ -69,79 +64,32 @@ if __name__ == '__main__':
     print('Loading cost: %.6f seconds' %(time.clock() - start_time))
     print('Load data successful')
 
-    # load custom dataset:
-    gt_directory = './with_gt'
+    #algorithm
+    print(data.head(10))
+    if args.ground_truth:
+        alg_selector = Cash(data, ground_truth)
+    else:
+        alg_selector = Cash(data, None)
+    print('Start AutoML:')
+    start_time = time.clock()
+    clf = alg_selector.model_selector(max_evals=50)
+    end_time = time.clock()
+    print('End AutoML:')
 
-    result_table = pd.DataFrame(columns=['data','Prec','Recall','F1','ROC','time','model'])
+    clf.fit(data)
+    prediction_result = clf.predict(data)
+    outlierness = clf.decision_function(data)
 
-    for file in os.listdir(gt_directory):
-        print(file)
-        data = pd.read_csv(os.path.join(gt_directory,file))
-        data['value'] = data['value'].astype('float')
-        ground_truth = data['label'].values
-        data.drop(['label','timestamp','Unnamed: 0'],axis=1,inplace=True)
-        #print(data.columns,data.shape,ground_truth.shape)
+    if args.ground_truth:
+        output_performance(args.algorithm,ground_truth,prediction_result,end_time - start_time,outlierness)
 
-        #algorithm
-        #print(data.head(10))
-        if args.ground_truth:
-            alg_selector = Cash(data, ground_truth)
+    if args.visualize_distribution and args.ground_truth:
+        if not args.time_stamp:
+            visualize_distribution_static(data,prediction_result,outlierness,args.saving_path)
+            visualize_distribution(data,prediction_result,outlierness,args.saving_path)
+            visualize_outlierscore(outlierness,prediction_result,args.contamination,args.saving_path)
         else:
-            alg_selector = Cash(data, None)
+            visualize_distribution_time_serie(clf.ts,data,args.saving_path)
+            visualize_outlierscore(outlierness,prediction_result,args.contamination,args.saving_path)
 
-
-        clf , results = alg_selector.model_selector(max_evals=50)
-
-        # num_samples = data.shape[0]
-        # split = int(num_samples * 0.75)
-        # train_data, test_data = data.iloc[:split], data.iloc[split:]
-        # if args.ground_truth: ground_truth = ground_truth[split:]
-
-        print('Start processing:')
-        start_time = time.clock()
-        clf.fit(data)
-        prediction_result = clf.predict(data)
-        outlierness = clf.decision_function(data)
-
-        # clf.fit(train_data)
-        # prediction_result = clf.predict(test_data)
-        # outlierness = clf.decision_function(test_data)
-        print('Auto ML complete')
-        results += "\n\n>>> >>> >>> >>> >>> >>> >>> === >>> >>> >>> >>> >>> >>> >>>\n\nFINAL RESULT AFTER RETRAINING\n"
-
-        if args.ground_truth:
-
-            f = io.StringIO()
-            with contextlib.redirect_stdout(f):
-                output_performance(clf, ground_truth, prediction_result,
-                                   time.clock() - start_time, outlierness)
-            #print(f.getvalue())
-            results += f.getvalue()
-            f.close()
-
-            acc = accuracy_score(ground_truth, prediction_result)
-            prec = precision_score(ground_truth, prediction_result)
-            rec = recall_score(ground_truth, prediction_result)
-            f1 = f1_score(ground_truth, prediction_result)
-            roc = max(roc_auc_score(ground_truth, outlierness),
-                                            1 - roc_auc_score(ground_truth,
-                                                              outlierness))
-
-            with open('./results/results'+str(file)+'.txt','w') as f:
-                f.write(results)
-            row = [file,prec,rec,f1,roc,time.clock() - start_time,str(clf)]
-            result_table.loc[len(result_table)] = row
-
-        print('Final result complete')
-        # if args.visualize_distribution and args.ground_truth:
-        #     if not args.time_stamp:
-        #         visualize_distribution_static(data,prediction_result,outlierness,args.saving_path)
-        #         visualize_distribution(data,prediction_result,outlierness,args.saving_path)
-        #         visualize_outlierscore(outlierness,prediction_result,args.contamination,args.saving_path)
-        #     else:
-        #         visualize_distribution_time_serie(clf.ts,data,args.saving_path)
-        #         visualize_outlierscore(outlierness,prediction_result,args.contamination,args.saving_path)
-        # print('Visuals complete ')
-
-    result_table.to_csv('HEY_THR_ec2.csv')
     conn.close()
